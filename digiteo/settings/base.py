cecb8099b19bde75052d016d5160441c2abf4604
haram8009 <haram8009@gmail.com>
2023-07-31 21:15:24 +0900

    settings 수정

--- digiteo/settings/base.py
+++ digiteo/settings/base.py
@@ -31,17 +31,23 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # my app
+    'account',
 
+    # django rest_framework
+    'rest_framework',
+
+    # jwt
+
+    # CORS
     'corsheaders',
 
-    'account',
 ]
 
 MIDDLEWARE = [
@@ -109,9 +115,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
+
 
 USE_I18N = True
 
