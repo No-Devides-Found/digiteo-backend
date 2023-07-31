81e2d5bdf48e8e379525c832458eb4c8d8929cf5
haram8009 <haram8009@gmail.com>
2023-07-31 20:53:20 +0900

    cors 해결

--- digiteo/settings/base.py
+++ digiteo/settings/base.py
@@ -13,7 +13,7 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -39,10 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'corsheaders',
+
     'account',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
--- digiteo/settings/local.py
+++ digiteo/settings/local.py
@@ -1,4 +1,8 @@
 from .base import *
 
 DEBUG = True  # 로컬에서 True 프로덕션에선 False
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+
+# CORS 설정 - whitelist 에 추가된 주소 접근 허용
+CORS_ORIGIN_WHITELIST = ['http://127.0.0.1:3000', 'http://localhost:3000']
+CORS_ALLOW_CREDENTIALS = True
