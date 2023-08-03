3d72489a63bfde5c9c4abd50d53c75f17e9cdf70
gpffh20 <eunbin584836@gmail.com>
2023-08-04 02:40:31 +0900

    feat: programs 앱 추가

--- accounts/apps.py
+++ accounts/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class AccountConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
+
+class ProgramConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'programs'
+
+
