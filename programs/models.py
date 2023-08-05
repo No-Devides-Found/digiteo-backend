112650867eedd9d0ece798380c25512771c61277
eushin <eunbin584836@gmail.com>
2023-08-05 20:48:20 +0900

    temp: 모델 및 시리얼라이저 만드는 중

--- programs/models.py
+++ programs/models.py
@@ -1,11 +1,33 @@
 from django.db import models
 
 class Program(models.Model):
-    title = models.CharField(max_length=30, unique=True)
-    introduce = models.TextField
-    review = models.TextField(default="")
-    # 추후 수정: assginment 모델 만들고나서 수정
-    assignment_id = models.ForeignKey("", on_delete=models.CASCADE)
+    title = models.CharField(max_length=50, unique=True)
+    introduce = models.TextField(max_length=500)
+    review = models.TextField(max_length=500, default="")
+    assignment_id = models.ForeignKey("Assignment", on_delete=models.CASCADE)
     thumbnail = models.ImageField
-    
+    category_id = models.ForeignKey("Category", on_delete=models.CASCADE)
 
+
+class Category(models.Model):
+    # 카테고리 명
+    category = models.CharField(max_length=10, unique=True)
+
+
+#  논의 필요
+# class Quiz(models.Model):
+#     program_id = models.ForeignKey("Program", on_delete=models.CASCADE)
+#     question = models.TextField(max_length=500)
+#     passage = models.TextField(max_length=500, default="")
+#     answer = 
+
+class Contents(models.Model):
+    program_id = models.ForeignKey("Program", on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    content = models.TextField(max_length=500)
+    order = models.IntegerField
+
+
+# 논의 필요
+# class Assignment(models.Model):
+#     pass
--- programs/sefializers.py
+++ programs/sefializers.py
@@ -1,3 +1,28 @@
 from rest_framework import serializers
-# from .models import 
+from .models import Program, Category
+# ,Assignment
 
+class ProgramSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Program
+		fields = '__all__'
+
+
+class CategorySerializer(serializers.ModelSerializer):
+	programs = ProgramSerializer(many=True, read_only=True)
+	
+	def create(self, validated_data):
+		program = Program.objects.create(**validated_data)
+		return program
+
+	class Meta:
+		model = Category
+		fields = '__all__'
+
+
+# class AssignmentSerializer(serializers.ModelSerializer):
+# 	programs = ProgramSerializer(many=True, read_only=True)
+	
+# 	class Meta: 
+# 		model = Assignment
+# 		fields = '__all__'
