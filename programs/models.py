3809b0a8dba6eaee29987ddf7e516b42acc6973d
gpffh20 <eunbin584836@gmail.com>
2023-08-05 15:42:01 +0900

    temp: programs 모델 만드는 중

--- programs/models.py
+++ programs/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Program(models.Model):
+    title = models.CharField(max_length=30, unique=True)
+    introduce = models.TextField
+    review = models.TextField(default="")
+    # 추후 수정: assginment 모델 만들고나서 수정
+    assignment_id = models.ForeignKey("", on_delete=models.CASCADE)
+    thumbnail = models.ImageField
+    
+
