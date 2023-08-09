03dac2df3e5847565ee55c8b2e3dac35bec5584b
eushin <eunbin584836@gmail.com>
2023-08-09 13:44:53 +0900

    fix: 카테고리 아이디 대신 카테고리 명 뜨게 변경

--- programs/views.py
+++ programs/views.py
@@ -6,3 +6,28 @@ class ProgramViewSet(viewsets.ModelViewSet):
 	queryset = Program.objects.all()
 	serializer_class = ProgramSerializer
 
+
+class CategoryViewSet(viewsets.ModelViewSet):
+	queryset = Category.objects.all()
+	serializer_class = CategorySerializer
+
+
+class ContentsViewSet(viewsets.ModelViewSet):
+	queryset = Contents.objects.all()
+	serializer_class = ContentsSerializer
+
+
+class QuizViewSet(viewsets.ModelViewSet):
+	queryset = Quiz.objects.all()
+	serializer_class = QuizSerializer
+
+
+class AssignmentViewSet(viewsets.ModelViewSet):
+	queryset = Assignment.objects.all()
+	serializer_class = AssignmentSerializer
+
+
+class TagsViewSet(viewsets.ModelViewSet):
+	queryset = Tags.objects.all()
+	serializer_class = TagsSerializer
+
