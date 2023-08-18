f64bb11932f864cd8d87bc45eea39763a823b279
haram8009 <haram8009@gmail.com>
2023-08-19 00:14:29 +0900

    prod: cors origin setting

--- digiteo/settings/production.py
+++ digiteo/settings/production.py
@@ -3,6 +3,11 @@ from .base import *
 DEBUG = False  # 로컬에서 True 프로덕션에선 False
 ALLOWED_HOSTS = ['43.202.94.215', 'api.digiteo.co.kr']
 
+CORS_ORIGIN_WHITELIST = ['https://digiteo.co.kr', 'https://www.digiteo.co.kr']
+CORS_ALLOW_CREDENTIALS = True
+CSRF_TRUSTED_ORIGINS = [
+    'https://api.digiteo.co.kr/', 'https://api.digiteo.co.kr/']
+
 # 프로덕션만
 STATIC_ROOT = BASE_DIR / 'static'
 MEDIA_ROOT = BASE_DIR / 'media'
